f3b7ac8a7aae9d84c16fe6e50755aa2c15196842
Mitchell Borchers <30349201+borchr27@users.noreply.github.com>
2022-06-07 17:03:06 +0200

    add delete method, and other support fn's, minor refactor

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ class AVLTree<T> where T : IComparable<T>{
         num_nodes = 0;
     }
 
-    private int node_height(Node? node){
+    public int node_height(Node? node){
         return node == null ? -1 : node.height;
     }
 
@@ -39,22 +39,20 @@ class AVLTree<T> where T : IComparable<T>{
         return node_height(node.left) - node_height(node.right);
     }
 
-    public T minimum() {
-        // find the minimum value within the avl tree
-        Node n = root!;
+    public Node minimum(Node n) {
+        // find the minimum node within the avl tree
         while(n.left != null){
             n = n.left;
         }
-        return n.val;
+        return n;
     }
 
-    public T maximum() {
+    public Node maximum(Node n) {
         // find the maximum value within the avl tree
-        Node n = root!;
         while(n.right != null){
             n = n.right;
         }
-        return n.val;
+        return n;
     }
 
     public bool find(T val){
@@ -79,7 +77,7 @@ class AVLTree<T> where T : IComparable<T>{
         return false;
     }
 
-    public void rightRotate(Node x) {
+    public void right_rotate(Node x) {
         // separate the right(parent) and left(child) nodes and make the L node the parent and the R node the child (CW rotation)
         //       x 
         //     y   c
@@ -116,7 +114,7 @@ class AVLTree<T> where T : IComparable<T>{
         y.height = 1 + Max(node_height(y.left), node_height(y.right));
     }
 
-    public void leftRotate(Node x) {
+    public void left_rotate(Node x) {
         // same idea as for the right rotate except it is mirrored (CCW rotation)
         Node y = x.right!;
         x.right = y.left;
@@ -177,20 +175,20 @@ class AVLTree<T> where T : IComparable<T>{
                 if(y == x_node.right) {
                     // y is left child of x and z is left child of y - handles LL and L case
                     if (z == x_node.right.right) {
-                        leftRotate(x_node);
+                        left_rotate(x_node);
                     // y is right child of x and z is left child of y - RL case
                     } else if (z == x_node.right.left) { 
-                        rightRotate(y);
-                        leftRotate(x_node);
+                        right_rotate(y);
+                        left_rotate(x_node);
                     }
                 } else if(y == x_node.left) {
                     // y is right child of x and z is right child of y - handles RR and R case
                     if(z == x_node.left.left) {
-                        rightRotate(x_node);
+                        right_rotate(x_node);
                     // if y is left child of x and z is right child of y - LR case
                     } else if(z == x_node.left.right) {
-                        leftRotate(y);
-                        rightRotate(x_node);
+                        left_rotate(y);
+                        right_rotate(x_node);
                     }
                 }
                 break;
@@ -206,21 +204,165 @@ class AVLTree<T> where T : IComparable<T>{
         return num_nodes;
     }
 
-    public void print(Node node){
+    public void print(Node? node) {
         // Pass a node to print values and smbols of wether L or R of node, prints a very rough tree best for small test cases
-        WriteLine($"{node.val}");
+        if (node == null){
+            WriteLine("empty tree!");
+        } else {
+            WriteLine($"{node.val}");
 
-        if (node.right != null){
-            WriteLine("\\");
-            print(node.right);
-        }
-        if (node.left != null){
-            WriteLine("/");
-            print(node.left);
+            if (node.right != null){
+                WriteLine("\\");
+                print(node.right);
+            }
+            if (node.left != null){
+                WriteLine("/");
+                print(node.left);
+            }
         }
     }
 
-    // TODO Delete Method
+    public void ordered_print(Node n){
+        if (n != null){
+            ordered_print(n.left);
+            Write($"{n.val} ");
+            ordered_print(n.right);
+        }
+    }
+
+    public void move_up(Node u, Node v) {
+        // helper method to rearrange nodes
+        if (u.parent == null){
+            // here u is root
+            root = v;
+        } else if (u == u.parent.left){ 
+            // here u is left child
+            u.parent.left = v;
+        } else { 
+            // here u is right child
+            u.parent.right = v;
+        }
+
+        if (v != null) {
+            v.parent = u.parent;
+        }
+    }
+
+    public void delete_rebalance(Node node) {
+        // balancing method for deletion
+        Node? p = node;
+
+        while (p != null) {
+            p.height = 1 + Max(node_height(p.left), node_height(p.right));
+            // parent of the parent is unbalanced 
+            if (calculate_balance(p) <= -2 || calculate_balance(p) >= 2) {
+                Node x_node, y, z;
+                x_node = p!;
+                
+                // here the taller child of x is y
+                if (x_node.left.height > x_node.right.height) {
+                    y = x_node.left;
+                } else {
+                    y = x_node.right;
+                }
+                
+                // here the taller child of y is z
+                if (y.left.height > y.right.height) {
+                    z = y.left;
+                } else if (y.left.height < y.right.height) {
+                    z = y.right;
+                } else { 
+                    // here they are the same height, take a single rotation
+                    if (y == x_node.left) {
+                        z = y.left;
+                    } else {
+                        z = y.right;
+                    }
+                }
+
+                if(y == x_node.right) {
+                    // y is left child of x and z is left child of y - handles LL and L case
+                    if (z == x_node.right.right) {
+                        left_rotate(x_node);
+                    // y is right child of x and z is left child of y - RL case
+                    } else if (z == x_node.right.left) { 
+                        right_rotate(y);
+                        left_rotate(x_node);
+                    }
+                } else if(y == x_node.left) {
+                    // y is right child of x and z is right child of y - handles RR and R case
+                    if(z == x_node.left.left) {
+                        right_rotate(x_node);
+                    // if y is left child of x and z is right child of y - LR case
+                    } else if(z == x_node.left.right) {
+                        left_rotate(y);
+                        right_rotate(x_node);
+                    }
+                }
+            }
+            p = p.parent;
+        }
+    }
+
+
+    public bool delete_node(T val) {
+        // delete main driver function
+        if (root == null) {
+            return false;
+        }
+
+        Node? n = root!;
+        Node? z = null;
+        // find the node to delete
+        while (n != null){
+            if (val.CompareTo(n.val) > 0){
+                n = n.right;
+            } else if (val.CompareTo(n.val) < 0){
+                n = n.left;
+            } else if (val.CompareTo(n.val) == 0){
+                z = n;
+                break;
+            } else {
+                return false;
+            }
+        }
+
+        // if there is no left subtree to the deleted node then 
+        // move the right subtree up and reblance
+        if (z.left == null) {
+            move_up(z, z.right);
+            if (z.right != null) {
+                delete_rebalance(z.right);
+            }
+        // if there is no right subtree to the deleted node then 
+        // move the left subtree up and reblance
+        } else if (z.right == null) {
+            move_up(z, z.left);
+            if (z.left != null) {
+                delete_rebalance(z.left);
+            }
+        } else {
+            // two child node case, get min node in right subtree and move up
+            // set right and left child, rebalance
+            Node y = minimum(z.right); 
+            if (y.parent != z) {
+                move_up(y, y.right);
+                y.right = z.right;
+                y.right.parent = y;
+            }
+
+            move_up(z, y);
+            y.left = z.left;
+            y.left.parent = y;
+            if (y != null) {
+                delete_rebalance(y);
+            }
+        }
+
+        num_nodes -= 1;
+        return true;
+    }
+
 
 }
 
@@ -228,12 +370,34 @@ class AVLTree<T> where T : IComparable<T>{
 
 class Top{
     static void Main(){
+        basic_I_O_test();
+    }
+
+    static void basic_I_O_test(){
         var tree = new AVLTree<int>();
         tree.insert(10);
         tree.insert(15);
         tree.insert(13);
         tree.print(tree.root!);
+        tree.ordered_print(tree.root!);
+        WriteLine();
         WriteLine(tree.count_nodes());
-        WriteLine(tree.root!.height);
+        WriteLine(tree.node_height(tree.root));
+        WriteLine("------");
+        WriteLine(tree.delete_node(13));
+        tree.print(tree.root!);
+        WriteLine(tree.count_nodes());
+        WriteLine(tree.node_height(tree.root));
+        WriteLine("------");
+        WriteLine(tree.delete_node(10));
+        tree.print(tree.root!);
+        WriteLine(tree.count_nodes());
+        WriteLine(tree.node_height(tree.root));
+        WriteLine("------");
+        WriteLine(tree.delete_node(15));
+        tree.print(tree.root!);
+        WriteLine(tree.count_nodes());
+        WriteLine(tree.node_height(tree.root));
     }
+
 }
